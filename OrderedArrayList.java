a65d1fa2c249e2da22956ac9691ccbafcca0b466
wlin10 <wlin10@stuy.edu>
2020-12-14 00:07:00 -0500

    correctIndex

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,5 +7,37 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
-  
+
+  private int correctIndex(T element) {
+    int index = 0;
+    for (int i = 0; i < size(); i++) {
+      if (element.compareTo(get(index + 1)) > 0) {
+        index++;
+      }
+    }
+    return index;
+  }
+
+  public boolean add(T element) {
+    if (element == null){
+      throw new IllegalArgumentException("no null elements should be added");
+    }
+    super.add(element);
+    return true;
+  }
+
+  public void add(int index, T element) {
+    if (element == null){
+      throw new IllegalArgumentException("no null elements should be added");
+    }
+    super.add(index, element);
+  }
+
+  public T set(int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException("no null elements should be added");
+    }
+    return (super.set(index, value));
+  }
+
 }
