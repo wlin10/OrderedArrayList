d0f2f3055b2aa0cb3f9d73a5eba4a6fc35a1e08d
wlin10 <wlin10@stuy.edu>
2020-12-13 21:58:01 -0500

    constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,20 +1,28 @@
 public class NoNullArrayList<T> extends ArrayList<T>{
 
+  public NoNullArrayList() {
+    super();
+  }
+
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
   public boolean add(T element) {
     if (element == null){
-      throw IllegalArgumentException("no null elements should be added");
+      throw new IllegalArgumentException("no null elements should be added");
     }
   }
 
   public void add(int index, T element) {
     if (element == null){
-      throw IllegalArgumentException("no null elements should be added");
+      throw new IllegalArgumentException("no null elements should be added");
     }
   }
 
   public T set(int index, T element){
     if (element == null){
-      throw IllegalArgumentException("no null elements should be added");
+      throw new IllegalArgumentException("no null elements should be added");
     }
   }
 
