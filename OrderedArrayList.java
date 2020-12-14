10b91b7a1621cb4b99f5b13d7ce85b224828c46c
wlin10 <wlin10@stuy.edu>
2020-12-13 22:30:33 -0500

    constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,11 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+
+  public OrderedArrayList() {
+    super();
+  }
+
+  public OrderedArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+  
+}
